a5bf13bb07e9c5d832fb9e7ac99e4aa75384e6d5
Vadim Karasik <vadkarasik@gmail.com>
2022-10-04 12:15:49 +0300

    feat: add buffer

--- Fw/core/application.php
+++ Fw/core/application.php
@@ -13,4 +13,32 @@ class Application
     private $template = null;
 
     use \Fw\Core\Traits\Singleton;
+
+    public function header()
+    {
+        $this->startBuffer();
+        require 'Fw/templates/manao/header.php';
+    }
+
+    public function footer()
+    {
+        require 'Fw/templates/manao/footer.php';
+        $this->endBuffer();
+    }
+
+    private function startBuffer()
+    {
+        ob_start();
+    }
+
+    private function endBuffer()
+    {
+        $content = ob_get_contents();
+        $this->restartBuffer();
+    }
+
+    private function restartBuffer()
+    {
+        ob_end_clean();
+    }
 }
