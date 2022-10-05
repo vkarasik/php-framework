f8693d9d1c64a0191125fd81c0366a503a228834
Vadim Karasik <vadkarasik@gmail.com>
2022-10-05 15:09:20 +0300

    feat: add base content

---
+++ Fw/templates/manao/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php $this->pager->showHead(); ?>
+    <title><?php $this->pager->showProperty('title'); ?></title>
+</head>
+
+<body>
+    <h1><?php $this->pager->showProperty('head'); ?></h1>
