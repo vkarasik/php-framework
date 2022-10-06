0fe6a49fcddfc7477df208fe4efd10514ee5bffc
Vadim Karasik <vadkarasik@gmail.com>
2022-10-06 17:08:52 +0300

    refactor: move header init

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 require './Fw/init.php';
 $pager = $app->pager;
+$app->header();
 $pager->addCss('style.css');
 $pager->addJs('script.js');
 $pager->addJs('script.js');
@@ -8,7 +9,6 @@ $pager->addJs('script.min.js');
 $pager->addString('meta name="viewport" content="width=device-width, initial-scale=1.0"');
 $pager->setProperty('title', 'Main Page Title');
 $pager->setProperty('head', 'Page Head');
-$app->header();
 ?>
 
 <pre>
