b5a9fc8a7156375c8f77cb559fcd6054b51fc99b
Vadim Karasik <vadkarasik@gmail.com>
2022-10-20 09:48:16 +0300

    feat: add Server and Request classes

--- Fw/core/application.php
+++ Fw/core/application.php
@@ -6,19 +6,25 @@
 
 namespace Fw\Core;
 
+use Fw\Core\Type\Request;
+use Fw\Core\Type\Server;
+
 class Application
 {
     private $__components = [];
     public $pager = null;
     private $template = null;
+    private $request = null;
+    private $server = null;
 
     use \Fw\Core\Traits\Singleton;
 
     private function __construct()
     {
         $this->pager = Page::getInstance();
+        $this->request = new Request();
+        $this->server = new Server();
     }
-
 
     public function header()
     {
@@ -47,5 +53,15 @@ class Application
     private function restartBuffer()
     {
         ob_end_clean();
+    }
+
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    public function getServer()
+    {
+        return $this->server;
     }
 }
---
+++ Fw/core/type/request.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Request Class
+ */
+namespace Fw\Core\Type;
+
+use Fw\Core\Type\Dictionary;
+
+class Request extends Dictionary
+{
+    public function __construct()
+    {
+    }
+}
