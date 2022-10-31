82b6f89e6ef80b3de3cf9cb181224c02c147ef11
Vadim Karasik <vadkarasik@gmail.com>
2022-10-31 21:01:29 +0300

    refactor: extract text input component

---
+++ Fw/components/fw/interface.form.text/class.php
@@ -0,0 +1,17 @@
+<?php
+use Fw\Core\Component\Base;
+
+class InterfaceFormText extends Base
+{
+    public function __construct($id, $template, $params, $path)
+    {
+        parent::__construct($id, $template, $params, $path);
+    }
+
+    public function executeComponent()
+    {
+        $arrResult = array();
+        $this->result = $arrResult;
+        $this->template->render();
+    }
+}
---
+++ Fw/components/fw/interface.form.text/templates/default/template.php
@@ -0,0 +1,15 @@
+<?php
+$params = $this->component->params;
+$result = $this->component->result;
+$type = $params['type'];
+$name = $params['name'];
+$id = hrtime(true);
+$class = isset($params['additional_class']) ? $params['additional_class'] : '';
+$title = isset($params['title']) ? $params['title'] : '';
+$attr = isset($params['attr']) ? getAttr($params['attr']) : '';
+$default = isset($params['default']) ? $params['default'] : '';
+?>
+<div class="mb-3">
+    <label for="<?=$id?>" class="form-label"><?=$title?></label>
+    <input type="<?=$type?>" name="<?=$name?>" class="form-control <?=$class?>" id="<?=$id?>" <?=$attr?> placeholder="<?=$default?>">
+</div>
