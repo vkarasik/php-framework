11031c8deee5fb5f9321185ab9e0be147bf08b66
Vadim Karasik <vadkarasik@gmail.com>
2022-10-31 12:30:42 +0300

    refactor: change component styling an layout

--- Fw/components/fw/element.list/templates/default/template.php
+++ Fw/components/fw/element.list/templates/default/template.php
@@ -5,13 +5,17 @@ $limit = $params["limit"];
 $count = 0;
 ?>
 <div>
-    <h2>Component</h2>
-    <?php while ($count < $limit && isset($result[$count])): ?>
-    <?php if ($params["show_title"] == "Y"): ?>
-        <h3><?=$result[$count]["title"]?></h3>
-    <?php endif;?>
-        <p><?=$result[$count]["body"]?></p>
-        <span><?=$result[$count]["date"]?></span>
-    <?php $count++;?>
-    <?php endwhile;?>
+    <h2 class="display-4 mb-4">Element List Component</h2>
+    <div class="row mb-5">
+        <?php while ($count < $limit && isset($result[$count])): ?>
+            <div class="col-3">
+            <?php if ($params["show_title"] == "Y"): ?>
+                <h3><?=$result[$count]["title"]?></h3>
+            <?php endif;?>
+                <p><?=$result[$count]["body"]?></p>
+                <span><?=$result[$count]["date"]?></span>
+            <?php $count++;?>
+        </div>
+        <?php endwhile;?>
+    </div>
 </div>
